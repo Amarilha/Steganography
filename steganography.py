752ea9e9baebd44909dffbbd1e9cde994ed163c2
Igor Amarilha <120053865+Amarilha@users.noreply.github.com>
2023-01-15 17:30:01 +0100

    image

--- steganography.py
+++ steganography.py
@@ -5,18 +5,43 @@ from PIL import Image, ImageTk
 import os
 from stegano import lsb
 
+
+
 root=Tk()
 root.title("Steganography")
 root.geometry("700x500+250+180")
 root.resizable(False,False)
 root.configure(bg="#2f4155")
 
+def showimage():
+    global filename
+    filename=filedialog.askopenfilename(initialdir=os.getcwd(), title='Select Image File', filetypes=(("PNG file","*.png"),("JPG.file","*.jpg"),("All file","*.txt")))
+    
+    img=Image.open(filename)
+    img=ImageTk.PhotoImage(img)
+    lb1.configure(image=img,width=250,height=250)
+    lb1.image=img
+       
+def Hide():
+    global secret
+    Message=text1.get(1.0, END)
+    secre= lsb.hide(str(filename), Message)
+    
+def Show():
+    clear_message = lsb.reveal(filename)
+    Text.delete(1.0,END)
+    text1.insert(END, clear_message)
+    
+def save():
+    secret.save("hidden.png")
+
+
 #icon
-image_icon=PhotoImage(file="")
+image_icon=PhotoImage(file="image/investigacao.png")
 root.iconphoto(False,image_icon)
 
 #logo
-logo=PhotoImage(file="")
+logo=PhotoImage(file="image/digitalizacao-de-impressao-digital.png")
 Label(root,image=logo, bg="#2f4155").place(x=10,y=0)
 
 Label(root,text="CYBER SCIENCE",bg="#2d4155",fg="white",font="arial 25 bold").place(x=100,y=20)
@@ -41,6 +66,22 @@ scrollbar1.place(x=320,y=0,height=300)
 scrollbar1.configure(command=text1.yview)
 text1.configure(yscrollcommand=scrollbar1.set)
 
+#third Frame
+frame3=Frame(root,bd=3,bg="#2f4155", width=330,height=100,relief=GROOVE)
+frame3.place(x=10,y=370)
+
+Button(frame3,text="Open Image",width=10,height=2,font="arial 14 bold",command=showimage).place(x=20, y=30)
+Button(frame3,text="Save Image",width=10,height=2,font="arial 14 bold",command=save).place(x=180, y=30)
+Label(frame3,text="Picture, Image, Photo file",bg="#2f4155",fg='yellow').place(x=20,y=5)
+
+
+#fourth Frame
+frame4=Frame(root,bd=3,bg="#2f4155", width=330,height=100,relief=GROOVE)
+frame4.place(x=3600,y=370)
+
+Button(frame4,text="Hide Data",width=10,height=2,font="arial 14 bold",command=Hide).place(x=20, y=30)
+Button(frame4,text="Show Data",width=10,height=2,font="arial 14 bold",command=Show).place(x=180, y=30)
+Label(frame4,text="Picture, Image, Photo file",bg="#2f4155",fg='yellow').place(x=20,y=5)
 
 
 
